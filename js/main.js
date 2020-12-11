896b4126ccd7c8c2d74098c4e9789e859605562a
MOSETI <enock.g.moseti@gmail.com>
2020-12-11 08:13:37 +0300

    update header and footer

--- js/main.js
+++ js/main.js
@@ -4,7 +4,22 @@ AOS.init({
     once: true
 });
 
+$('.carousel').carousel({
+
+    interval: 2000
+});
+
+
 jQuery(document).ready(function($) {
 
    "use strict";
 });
+const navbarToggler = document.querySelector('.navbar-toggler');
+
+navbarToggler.addEventListener('click', showMenu);
+
+function showMenu () {
+    if(!showMenu) {
+
+    }
+}
